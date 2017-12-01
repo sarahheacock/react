75f027de2da749dd80afe79672515cff35d86edb
sarahheacock <seheacock@bellsouth.net>
2017-11-30 20:15:02 -0500

    changed file

--- build/server/index.js
+++ build/server/index.js
@@ -24,7 +24,6 @@ var renderFullPage = function renderFullPage(html, preloadedState) {
   var src = "./index.js";
   var href = '';
   // '<link href="/index.css" rel="stylesheet">';
-  // <script type="text/javascript" src="/socket.js"></script>
 
   return '\n    <!DOCTYPE html>\n    <html>\n      <head>\n        <meta charset="utf-8">\n        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">\n        <meta name="theme-color" content="#000000">\n        ' + href + '\n        <title>Your SSR React Router Node app initialized with data!</title>\n      </head>\n      <body>\n        <div id="root">' + (html || 'Hello, World!') + '</div>\n        <script>\n          window.__PRELOADED_STATE__ = ' + JSON.stringify(preloadedState).replace(/</g, '||u003c') + '\n        </script>\n        <script type="text/javascript" src=' + src + '></script>\n      </body>\n    </html>\n  ';
 };

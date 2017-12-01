30ec2e9e2a9275f0f64618f70320b5657cc6a3a0
sarahheacock <seheacock@bellsouth.net>
2017-11-30 19:29:54 -0500

    git script

--- scripts/git.js
+++ scripts/git.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 
 const readline = require('readline');
-
+const fs = require('fs-extra');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -10,6 +10,7 @@ const rl = readline.createInterface({
 let response = '';
 
 function commit(deploy, callback){
+  console.log("\n\nPushing to:", deploy);
   exec(deploy, (err, stdout, stderr) => {
     if (err) {
       console.error(err);
@@ -20,33 +21,58 @@ function commit(deploy, callback){
   });
 }
 
+function ignore(deploy){
+  const file = ".gitignore";
+  const output = (deploy === "origin") ?
+  `
+    # dependencies
+    /node_modules
+
+    # testing
+    /coverage
+
+    /build
+  `:
+  `
+    # dependencies
+    /node_modules
+
+    # testing
+    /coverage
+
+    /src
+    /config
+    /test
+    /scripts
+  `;
+
+  fs.outputFileSync(file, output);
+  // .then(() => fs.readFile(file, 'utf8'))
+  // .then(data => {
+  //   console.log(data) // => hello!
+  // })
+  // .catch(err => {
+  //   console.error(err)
+  // });
+}
 
 function add(deploy){
-  // exec('git add --all', (err, stdout, stderr) => {
-  //   if (err) {
-  //     console.error(err);
-  //     return;
-  //   }
-  //   console.log(stdout);
-  //   console.log("add", deploy);
+  if(response){
+    commit(`git add --all && git commit -m "${response}" && git push ${deploy} master`, function(){
+      console.log("DONE")
+    });
+  }
+  else {
+    rl.question('commit note:\n ', (answer) => {
+      response = answer;
+      console.log(`\n\nCommiting changes: ${response}`);
+      rl.close();
 
-    if(response){
-      commit(`git push ${deploy} master`, function(){
-        console.log("DONE")
+      commit(`git add --all && git commit -m "${response}" && git push ${deploy} master`, function(){
+        add("heroku");
       });
-    }
-    else {
-      rl.question('commit note:\n ', (answer) => {
-        response = answer;
-        console.log(`\n\nCommiting changes: ${response}`);
-        rl.close();
-
-        commit(`git add --all && git commit -m "${response}" && git push ${deploy} master`, function(){
-          add("heroku");
-        });
-      });
-    }
-  // });
+    });
+  }
 }
 
 add("origin");

ee84ec584678ec0e3eb8c1d2f43724c18ae09af0
sarahheacock <seheacock@bellsouth.net>
2017-11-30 19:38:55 -0500

    rty

--- scripts/git.js
+++ scripts/git.js
@@ -23,48 +23,52 @@ function commit(deploy, callback){
 
 function ignore(deploy){
   const file = ".gitignore";
+  const same = `
+  # See https://help.github.com/ignore-files/ for more about ignoring files.
+
+  # dependencies
+  /node_modules
+
+  # testing
+  /coverage
+
+  # misc
+  .DS_Store
+  .env.local
+  .env.development.local
+  .env.test.local
+  .env.production.local
+
+  npm-debug.log*
+  yarn-debug.log*
+  yarn-error.log*
+  `;
+
   const output = (deploy === "origin") ?
   `
-    # dependencies
-    /node_modules
-
-    # testing
-    /coverage
-
-    /build
+  ${same}
+  /build
   `:
   `
-    # dependencies
-    /node_modules
-
-    # testing
-    /coverage
-
-    /src
-    /config
-    /test
-    /scripts
+  ${same}
+  /src
+  /config
+  /scripts
+  /test
   `;
 
   fs.outputFileSync(file, output);
-  // .then(() => fs.readFile(file, 'utf8'))
-  // .then(data => {
-  //   console.log(data) // => hello!
-  // })
-  // .catch(err => {
-  //   console.error(err)
-  // });
 }
 
 function add(deploy){
   if(response){
-    ignore(deploy);
+    // ignore(deploy);
     commit(`git add --all && git commit -m "${response}" && git push ${deploy} master`, function(){
       console.log("DONE")
     });
   }
   else {
-    ignore(deploy);
+    // ignore(deploy);
     rl.question('commit note:\n ', (answer) => {
       response = answer;
       console.log(`\n\nCommiting changes: ${response}`);
